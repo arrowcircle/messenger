6e6da4e8a85a05dc94eac0a4401c938b4a253c63
Oleg Bovykin <oleg.bovykin@gmail.com>
2015-12-04 17:36:11 +0300

    Cleaned up code with go-plus

--- main.go
+++ main.go
@@ -1,144 +1,155 @@
 package main
 
 import (
-  "github.com/spf13/viper"
-  "github.com/mattes/migrate/migrate"
-  "github.com/ant0ine/go-json-rest/rest"
-  _ "github.com/lib/pq"
-  "github.com/jinzhu/gorm"
-  "fmt"
-  "net/http"
-  "log"
-  "strconv"
-  "time"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/ant0ine/go-json-rest/rest"
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
+	"github.com/mattes/migrate/migrate"
+	"github.com/spf13/viper"
 )
 import _ "github.com/mattes/migrate/driver/postgres"
 
-type UserJson struct {
-  Id                  int        `json:"id"`
-  Name                string     `json:"name"`
+// UserJSON is used for empty requests
+type UserJSON struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
 }
 
-type DialogJson struct {
-  Id                  int          `json:"id"`
-  Name                string       `json:"name"`
-  CreatedAt           time.Time    `json:"created_at"`
-  UpdatedAt           time.Time    `json:"updated_at"`
-  UserIds             string       `json:"user_ids"`
-  LastMessage         string       `json:"last_message"`
-  LastMessageID       int          `json:"last_message_id"`
-  LastMessageUserID   int          `json:"last_message_user_id"`
-  LastSeenMessageID   int          `json:"last_seen_message_id"`
+// DialogJSON used for index action of API
+type DialogJSON struct {
+	ID                int       `json:"id"`
+	Name              string    `json:"name"`
+	CreatedAt         time.Time `json:"created_at"`
+	UpdatedAt         time.Time `json:"updated_at"`
+	UserIds           string    `json:"user_ids"`
+	LastMessage       string    `json:"last_message"`
+	LastMessageID     int       `json:"last_message_id"`
+	LastMessageUserID int       `json:"last_message_user_id"`
+	LastSeenMessageID int       `json:"last_seen_message_id"`
 }
 
-type DialogCreateJson struct {
-  Name                string       `json:"name"`
-  UserIds             []int        `json:"user_ids"`
-  Message             string       `json:"message"`
+// DialogCreateJSON is used for dialogs creation
+type DialogCreateJSON struct {
+	Name    string `json:"name"`
+	UserIds []int  `json:"user_ids"`
+	Message string `json:"message"`
 }
 
+// Dialog is used to save dialogs into DB via GORM
 type Dialog struct {
-  Id                  int           `json:"id"`
-  Name                string        `json:"name"`
-  LastMessageID       int           `json:"last_message_id"`
-  CreatedAt           time.Time     `json:"created_at"`
-  UpdatedAt           time.Time     `json:"updated_at"`
+	ID            int       `json:"id"`
+	Name          string    `json:"name"`
+	LastMessageID int       `json:"last_message_id"`
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
 }
 
-type DialogShowJson struct {
-  Id                  int              `json:"id"`
-  Name                string           `json:"name"`
-  Messages            []MessageJson    `json:"messages"`
+// DialogShowJSON is used to form json
+type DialogShowJSON struct {
+	ID       int           `json:"id"`
+	Name     string        `json:"name"`
+	Messages []MessageJSON `json:"messages"`
 }
 
-type MessageJson struct {
-  Id                  int          `json:"id"`
-  Text                string       `json:"text"`
-  CreatedAt           time.Time    `json:"created_at"`
-  UserId              int          `json:"user_id"`
+// MessageJSON is used to response message in JSON format
+type MessageJSON struct {
+	ID        int       `json:"id"`
+	Text      string    `json:"text"`
+	CreatedAt time.Time `json:"created_at"`
+	UserID    int       `json:"user_id"`
 }
 
+// Message is used to put messages in DB via GORM
 type Message struct {
-  Id                  int          `json:"id"`
-  Text                string       `json:"text"`
-  UserId              int          `json:"user_id"`
-  DialogId            int          `json:"dialog_id"`
+	ID       int    `json:"id"`
+	Text     string `json:"text"`
+	UserID   int    `json:"user_id"`
+	DialogID int    `json:"dialog_id"`
 }
 
 func main() {
-  readConfig()
-  applyMigrations()
-  i := Impl{}
-  i.connectToDb()
-  i.startChat()
+	readConfig()
+	applyMigrations()
+	i := Impl{}
+	i.connectToDb()
+	i.startChat()
 }
 
 func readConfig() {
-  viper.SetEnvPrefix("chat")
-  viper.SetDefault("database_url", "postgres:///chat_development?sslmode=disable")
-  viper.SetDefault("bind_address", "localhost:8080")
-  viper.AutomaticEnv()
-  viper.BindEnv("database_url")
-  viper.BindEnv("bind_address")
+	viper.SetEnvPrefix("chat")
+	viper.SetDefault("database_url", "postgres:///chat_development?sslmode=disable")
+	viper.SetDefault("bind_address", "localhost:8080")
+	viper.AutomaticEnv()
+	viper.BindEnv("database_url")
+	viper.BindEnv("bind_address")
 }
 
 func applyMigrations() {
-  allErrors, ok := migrate.UpSync(viper.GetString("database_url"), "./migrations")
-  fmt.Println("Database: ", viper.GetString("database_url"))
-  if !ok {
-    fmt.Println("Migratinos failed")
-    fmt.Println("driver: ", viper.GetString("database_url"))
-    fmt.Println("Errors: ", allErrors)
-  }
+	allErrors, ok := migrate.UpSync(viper.GetString("database_url"), "./migrations")
+	fmt.Println("Database: ", viper.GetString("database_url"))
+	if !ok {
+		fmt.Println("Migratinos failed")
+		fmt.Println("driver: ", viper.GetString("database_url"))
+		fmt.Println("Errors: ", allErrors)
+	}
 }
 
 func (i *Impl) connectToDb() {
-  var err error
-  i.DB, err = gorm.Open("postgres", viper.GetString("database_url"))
-  if err != nil {
-      log.Fatalf("Got error when connect database, the error is '%v'", err)
-  }
-  i.DB.LogMode(true)
+	var err error
+	i.DB, err = gorm.Open("postgres", viper.GetString("database_url"))
+	if err != nil {
+		log.Fatalf("Got error when connect database, the error is '%v'", err)
+	}
+	i.DB.LogMode(true)
 }
 
+// Impl used to provide handler to DB
 type Impl struct {
-  DB gorm.DB
+	DB gorm.DB
 }
 
 func (i *Impl) startChat() {
-  api := rest.NewApi()
-  api.Use(rest.DefaultDevStack...)
-  router, err := rest.MakeRouter(
-    rest.Get("/", Index),
-    rest.Get("/users/:user_id.json", UserShow),
-    rest.Get("/users/:user_id/dialogs.json", i.DialogIndex),
-    rest.Get("/users/:user_id/dialogs/:dialog_id/messages.json", i.MessageIndex),
-    rest.Get("/users/:user_id/dialogs/:dialog_id.json", i.DialogShow),
-    rest.Post("/users/:user_id/dialogs.json", i.DialogCreate),
-    rest.Post("/users/:user_id/dialogs/:dialog_id/messages.json", i.MessageCreate),
-  )
-  if err != nil {
-    log.Fatal(err)
-  }
-  api.SetApp(router)
-  fmt.Println("address: ", viper.GetString("bind_address"))
-  log.Fatal(http.ListenAndServe(viper.GetString("bind_address"), api.MakeHandler()))
+	api := rest.NewApi()
+	api.Use(rest.DefaultDevStack...)
+	router, err := rest.MakeRouter(
+		rest.Get("/", Index),
+		rest.Get("/users/:user_id.json", UserShow),
+		rest.Get("/users/:user_id/dialogs.json", i.DialogIndex),
+		rest.Get("/users/:user_id/dialogs/:dialog_id/messages.json", i.MessageIndex),
+		rest.Get("/users/:user_id/dialogs/:dialog_id.json", i.DialogShow),
+		rest.Post("/users/:user_id/dialogs.json", i.DialogCreate),
+		rest.Post("/users/:user_id/dialogs/:dialog_id/messages.json", i.MessageCreate),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	api.SetApp(router)
+	fmt.Println("address: ", viper.GetString("bind_address"))
+	log.Fatal(http.ListenAndServe(viper.GetString("bind_address"), api.MakeHandler()))
 }
 
+// Index is status function
 func Index(w rest.ResponseWriter, r *rest.Request) {
-  w.WriteJson("Welcome!\n")
+	w.WriteJson("Welcome!\n")
 }
 
+// DialogIndex is used to get dialogs index
 func (i *Impl) DialogIndex(w rest.ResponseWriter, r *rest.Request) {
-  userId := r.PathParam("user_id")
-  page, err := strconv.Atoi(r.FormValue("page"))
-  offset := 0
-  if err == nil {
-    offset = (page - 1) * 10
-  }
+	userID := r.PathParam("user_id")
+	page, err := strconv.Atoi(r.FormValue("page"))
+	offset := 0
+	if err == nil {
+		offset = (page - 1) * 10
+	}
 
-  dialogs := []DialogJson{}
-  i.DB.Raw(`
+	dialogs := []DialogJSON{}
+	i.DB.Raw(`
     SELECT c.*, array_agg(du.user_id) AS user_ids
     FROM
       (SELECT
@@ -166,122 +177,126 @@ func (i *Impl) DialogIndex(w rest.ResponseWriter, r *rest.Request) {
       c.last_seen_message_id
     LIMIT 10
     OFFSET ?
-  `, userId, offset).Find(&dialogs)
-  w.WriteJson(&dialogs)
+  `, userID, offset).Find(&dialogs)
+	w.WriteJson(&dialogs)
 }
 
+// DialogShow is used to show dialog for RAILS
 func (i *Impl) DialogShow(w rest.ResponseWriter, r *rest.Request) {
-  userId := r.PathParam("user_id")
-  dialogId, _ := strconv.Atoi(r.PathParam("dialog_id"))
-  page, err := strconv.Atoi(r.FormValue("page"))
-  offset := 0
-  if err == nil {
-    offset = (page - 1) * 10
-  }
-  dialog := DialogShowJson{}
-  dialog.Id = dialogId
-  i.DB.Raw(`
+	userID := r.PathParam("user_id")
+	dialogID, _ := strconv.Atoi(r.PathParam("dialog_id"))
+	page, err := strconv.Atoi(r.FormValue("page"))
+	offset := 0
+	if err == nil {
+		offset = (page - 1) * 10
+	}
+	dialog := DialogShowJSON{}
+	dialog.ID = dialogID
+	i.DB.Raw(`
     SELECT * FROM messages
     WHERE messages.dialog_id = ?
     ORDER BY messages.id DESC
     LIMIT 10
     OFFSET ?
-  `, dialogId, offset).Find(&dialog.Messages)
+  `, dialogID, offset).Find(&dialog.Messages)
 
-  last_message_id := 0
-  i.DB.Raw("SELECT last_message_id FROM dialogs WHERE id = ?", dialogId).Row().Scan(&last_message_id)
-  i.DB.Exec("UPDATE dialog_users SET last_seen_message_id = ? WHERE dialog_id = ? AND user_id = ?", last_message_id, dialogId, userId)
+	lastMessageID := 0
+	i.DB.Raw("SELECT last_message_id FROM dialogs WHERE id = ?", dialogID).Row().Scan(&lastMessageID)
+	i.DB.Exec("UPDATE dialog_users SET last_seen_message_id = ? WHERE dialog_id = ? AND user_id = ?", lastMessageID, dialogID, userID)
 
-  w.WriteJson(&dialog)
+	w.WriteJson(&dialog)
 }
 
+// MessageIndex is used to show dialog messages
 func (i *Impl) MessageIndex(w rest.ResponseWriter, r *rest.Request) {
-  userId := r.PathParam("user_id")
-  dialogId, _ := strconv.Atoi(r.PathParam("dialog_id"))
-  page, err := strconv.Atoi(r.FormValue("page"))
-  offset := 0
-  if err == nil {
-    offset = (page - 1) * 10
-  }
-  messages := []MessageJson{}
-  i.DB.Raw(`
+	userID := r.PathParam("user_id")
+	dialogID, _ := strconv.Atoi(r.PathParam("dialog_id"))
+	page, err := strconv.Atoi(r.FormValue("page"))
+	offset := 0
+	if err == nil {
+		offset = (page - 1) * 10
+	}
+	messages := []MessageJSON{}
+	i.DB.Raw(`
     SELECT * FROM messages
     WHERE messages.dialog_id = ?
     ORDER BY messages.id DESC
     LIMIT 10
     OFFSET ?
-  `, dialogId, offset).Find(&messages)
+  `, dialogID, offset).Find(&messages)
 
-  last_message_id := 0
-  i.DB.Raw("SELECT last_message_id FROM dialogs WHERE id = ?", dialogId).Row().Scan(&last_message_id)
-  i.DB.Exec("UPDATE dialog_users SET last_seen_message_id = ? WHERE dialog_id = ? AND user_id = ?", last_message_id, dialogId, userId)
+	lastMessageID := 0
+	i.DB.Raw("SELECT last_message_id FROM dialogs WHERE id = ?", dialogID).Row().Scan(&lastMessageID)
+	i.DB.Exec("UPDATE dialog_users SET last_seen_message_id = ? WHERE dialog_id = ? AND user_id = ?", lastMessageID, dialogID, userID)
 
-  w.WriteJson(&messages)
+	w.WriteJson(&messages)
 }
 
+// UserShow if fake method for rails
 func UserShow(w rest.ResponseWriter, r *rest.Request) {
-  userId, _ := strconv.Atoi(r.PathParam("user_id"))
-  user := UserJson{}
-  user.Id = userId
-
-  w.WriteJson(&user)
+	userID, _ := strconv.Atoi(r.PathParam("user_id"))
+	user := UserJSON{}
+	user.ID = userID
+	w.WriteJson(&user)
 }
 
+// DialogCreate is used to create dialog and message
 func (i *Impl) DialogCreate(w rest.ResponseWriter, r *rest.Request) {
-  dialogJson := DialogCreateJson{}
-  if err := r.DecodeJsonPayload(&dialogJson); err != nil {
-    fmt.Println("error decoding json: ", err)
+	dialogJSON := DialogCreateJSON{}
+	if err := r.DecodeJsonPayload(&dialogJSON); err != nil {
+		fmt.Println("error decoding json: ", err)
 		rest.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-  dialog := Dialog{}
-  dialog.Name = dialogJson.Name
+	dialog := Dialog{}
+	dialog.Name = dialogJSON.Name
 
 	if err := i.DB.Save(&dialog).Error; err != nil {
-    fmt.Println("error saving message: ", err)
+		fmt.Println("error saving message: ", err)
 		rest.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-  message := Message{}
-  message.DialogId = dialog.Id
-  message.Text = dialogJson.Message
+	message := Message{}
+	message.DialogID = dialog.ID
+	message.Text = dialogJSON.Message
 
-  if err := i.DB.Save(&message).Error; err != nil {
-    fmt.Println("error saving message: ", err)
+	if err := i.DB.Save(&message).Error; err != nil {
+		fmt.Println("error saving message: ", err)
 		rest.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-  for _, element := range dialogJson.UserIds {
-    i.DB.Exec("INSERT INTO dialog_users (dialog_id, user_id, last_seen_message_id) VALUES (?, ?, 0)", dialog.Id, element)
-  }
+	for _, element := range dialogJSON.UserIds {
+		i.DB.Exec("INSERT INTO dialog_users (dialog_id, user_id, last_seen_message_id) VALUES (?, ?, 0)", dialog.ID, element)
+	}
 
-  i.DB.Exec("UPDATE dialogs SET last_message_id = ? WHERE id = ?", message.Id, dialog.Id)
+	i.DB.Exec("UPDATE dialogs SET last_message_id = ? WHERE id = ?", message.ID, dialog.ID)
 
-  dialog.LastMessageID = message.Id
+	dialog.LastMessageID = message.ID
 
-  fmt.Println("dialog json: ", dialog.Id)
+	fmt.Println("dialog json: ", dialog.ID)
 
-  w.WriteJson(&dialog)
+	w.WriteJson(&dialog)
 }
 
+// MessageCreate creates message for dialog
 func (i *Impl) MessageCreate(w rest.ResponseWriter, r *rest.Request) {
-  message := Message{}
-  if err := r.DecodeJsonPayload(&message); err != nil {
-    fmt.Println("error decoding json: ", err)
+	message := Message{}
+	if err := r.DecodeJsonPayload(&message); err != nil {
+		fmt.Println("error decoding json: ", err)
 		rest.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	if err := i.DB.Save(&message).Error; err != nil {
-    fmt.Println("error saving message: ", err)
+		fmt.Println("error saving message: ", err)
 		rest.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-  i.DB.Exec("UPDATE dialogs SET last_message_id = ? WHERE dialogs.id = ?", message.Id, message.DialogId)
-  i.DB.Exec("UPDATE dialog_users SET last_seen_message_id = ? WHERE dialog_id = ? AND user_id = ?", message.Id, message.DialogId, message.UserId)
+	i.DB.Exec("UPDATE dialogs SET last_message_id = ? WHERE dialogs.id = ?", message.ID, message.DialogID)
+	i.DB.Exec("UPDATE dialog_users SET last_seen_message_id = ? WHERE dialog_id = ? AND user_id = ?", message.ID, message.DialogID, message.UserID)
 
-  w.WriteJson(&message)
+	w.WriteJson(&message)
 }
